a3b8013b1ed119a73d6a6625311cd386679d16d8
Vadym Myrgorod <dealancer@gmail.com>
2020-12-11 18:43:12 -0500

    Use multiple sources to determin location, default to Israel

--- js/scripts.js
+++ js/scripts.js
@@ -9,56 +9,81 @@ function scale() {
 }
 
 function light() {
+  var SunCalc = window.SunCalc;
+  var hanukkahFirstDayTime = new Date(year = hannukkahFirstDayYear, month = hannukkahFirstDayMonth - 1, date = hannukkahFirstDayDate - 1, seconds = 1);
+
+  var lat = 31.771959;
+  var lon = 35.217018;
+  hanukkahFirstDayTime = new Date(SunCalc.getTimes(hanukkahFirstDayTime, lat, lon).sunset.getTime() - 18*60000);
+
+  setTimeout(function(){ setLight(); }, 10000);
+  setInterval(function(){ setLight(); }, 60000);
+
+  fetch('https://extreme-ip-lookup.com/json/')
+  .then( res => res.json())
+  .then(response => {
+    lat = response.lat;
+    lon = response.lon;
+    hanukkahFirstDayTime = new Date(SunCalc.getTimes(hanukkahFirstDayTime, lat, lon).sunset.getTime() - 18*60000);
+    setLight();
+  });
+
   navigator.geolocation.getCurrentPosition(function(position) {
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-    var SunCalc = window.SunCalc;
-    var hanukkahFirstDayTime = new Date(year = hannukkahFirstDayYear, month = hannukkahFirstDayMonth - 1, date = hannukkahFirstDayDate - 1, seconds = 1);
-    var hanukkahFirstDayTime = new Date(SunCalc.getTimes(hanukkahFirstDayTime, lat, long).sunset.getTime() - 18*60000);
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    hanukkahFirstDayTime = new Date(SunCalc.getTimes(hanukkahFirstDayTime, lat, lon).sunset.getTime() - 18*60000);
+    setLight();
+  });
 
-    function setLight() {
-      var currentTime = new Date();
-      var hannukkahDay = Math.ceil((currentTime - hanukkahFirstDayTime) / 86400000);
+  function setLight() {
+    var currentTime = new Date();
+    var hannukkahDay = Math.ceil((currentTime - hanukkahFirstDayTime) / 86400000);
 
-      console.log("Day " + hannukkahDay);
+    console.log("Day " + hannukkahDay);
 
-      if (hannukkahDay >= 1 && hannukkahDay < 9) {
-        document.getElementById("five").style.visibility="visible";
-        document.getElementById("one").style.visibility="visible";
-      }
+    document.getElementById("one").style.visibility = "hidden";
+    document.getElementById("two").style.visibility = "hidden";
+    document.getElementById("three").style.visibility = "hidden";
+    document.getElementById("four").style.visibility = "hidden";
+    document.getElementById("five").style.visibility = "hidden";
+    document.getElementById("six").style.visibility = "hidden";
+    document.getElementById("seven").style.visibility = "hidden";
+    document.getElementById("eight").style.visibility = "hidden";
+    document.getElementById("nine").style.visibility = "hidden";
 
-      if (hannukkahDay >= 2  && hannukkahDay < 9) {
-        document.getElementById("two").style.visibility="visible";
-      }
-
-      if (hannukkahDay >= 3 && hannukkahDay < 9) {
-        document.getElementById("three").style.visibility="visible";
-      }
-
-      if (hannukkahDay >= 4 && hannukkahDay < 9) {
-        document.getElementById("four").style.visibility="visible";
-      }
-
-      if (hannukkahDay >= 5 && hannukkahDay < 9) {
-        document.getElementById("six").style.visibility="visible";
-      }
-
-      if (hannukkahDay >= 6 && hannukkahDay < 9) {
-        document.getElementById("seven").style.visibility="visible";
-      }
-
-      if (hannukkahDay >= 7 && hannukkahDay < 9) {
-        document.getElementById("eight").style.visibility="visible";
-      }
-
-      if (hannukkahDay >= 8 && hannukkahDay < 9) {
-        document.getElementById("nine").style.visibility="visible";
-      }
+    if (hannukkahDay >= 1 && hannukkahDay < 9) {
+      document.getElementById("five").style.visibility="visible";
+      document.getElementById("one").style.visibility="visible";
     }
 
-    setLight();
-    setInterval(function(){ setLight(); }, 60000);
-  });
+    if (hannukkahDay >= 2  && hannukkahDay < 9) {
+      document.getElementById("two").style.visibility="visible";
+    }
+
+    if (hannukkahDay >= 3 && hannukkahDay < 9) {
+      document.getElementById("three").style.visibility="visible";
+    }
+
+    if (hannukkahDay >= 4 && hannukkahDay < 9) {
+      document.getElementById("four").style.visibility="visible";
+    }
+
+    if (hannukkahDay >= 5 && hannukkahDay < 9) {
+      document.getElementById("six").style.visibility="visible";
+    }
+
+    if (hannukkahDay >= 6 && hannukkahDay < 9) {
+      document.getElementById("seven").style.visibility="visible";
+    }
+
+    if (hannukkahDay >= 7 && hannukkahDay < 9) {
+      document.getElementById("eight").style.visibility="visible";
+    }
+
+    if (hannukkahDay >= 8 && hannukkahDay < 9) {
+      document.getElementById("nine").style.visibility="visible";
+    }
+  }
 }
 
 window.onresize = scale;
